91a988822d948543f0b19815c5dbf7d8d7ffcdf9
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-07-26 19:31:47 +0000

    Test

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,15 +1,19 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from flask import Flask, current_app, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import JWTManager, create_access_token
+from models import db, User
+""" from api.utils import generate_sitemap, APIException"""
 from flask_cors import CORS
 
-api = Blueprint('api', __name__)
+api = Flask(__name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+jwt = JWTManager(api)
+api.config["JWT_SECRET_KEY"] = "super-duper-secret"
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -20,3 +24,7 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+if __name__ == '__main__':
+    PORT = 3001
+    api.run(host='0.0.0.0', port=PORT, debug=False)
